c1c41f71488bb0ff9bf5ae58fa85c6f9ebb699e6
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2018-02-22 18:55:38 -0300

    repo completo

--- 05_Digitos/digitos.cpp
+++ 05_Digitos/digitos.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 
 unsigned int cociente(unsigned int a, unsigned int b) {
-  if(b==0) throw 5;
-  if(a-b<=0)
-    return 0;
-  return 1+cociente(a-b,b);
+    if (b == 0) throw 5;
+    if (a - b <= 0)
+        return 0;
+    return 1 + cociente(a - b, b);
 }
 
 
 unsigned int resto(unsigned int a, unsigned int b) {
-  if(b==0) throw 5;
-  if(a<b)
-    return a;
-  return resto(a-b,b);
+    if (b == 0) throw 5;
+    if (a < b)
+        return a;
+    return resto(a - b, b);
 }
 
 
 void escribir_espaciado(unsigned int num) {
-  
+
 }
